cb533755f532c9ad121c220e059dd553f211d448
Cynox <97021216+cynox01@users.noreply.github.com>
2022-04-01 10:30:15 +0200

    ajoute dossier manquante

---
+++ src/components/Presentation.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Presentation() {
+  return (
+    <div className='card-presentation'>
+        
+            <div className='text-container'>
+                <h1 className='title-presetantion'>My Recette, <br/>toutes les recette du monde à ton cousine</h1>
+                <p className='text-presentation'>Recherchez et amusez-vous à fusionner toutes les recettes que nous avons pour vous.</p>
+            </div>
+    </div>
+  );
+}
+
+export default Presentation
