67ec89edffb0538bfe2b59ff264225cb0274c880
Cynox <97021216+cynox01@users.noreply.github.com>
2022-04-01 10:29:12 +0200

    premier Brian affichage accueil

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import './App.css'
-import Home from './pages/Home'
+import './assets/css/App.css';
+import Home from './pages/Home.js'
+
+import Presentation from './components/Presentation'
+import Navegator from './components/Navegator';
+import Cards from './components/Cards';
 
 function App() {
   return (
     <div className="App">
-    <Home/>
+      <Navegator/>
+      <Presentation/>
+      <Cards/>
     </div>
   );
 }
 
-export default App
+export default App;
