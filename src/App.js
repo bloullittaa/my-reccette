50541806aaebd0a2a63e50f03a10f2570f34efc0
Cynox <97021216+cynox01@users.noreply.github.com>
2022-04-04 10:43:55 +0200

    Error corrigz

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import './assets/css/App.css';
-import Home from './pages/Home.js'
-import Desserts from './pages/Desserts';
+import Presentation from './components/Presentation'
+import Navegator from './components/Navegator'
+import Cards from './components/Cards'
 
-import Presentation from './Components/Presentation'
-import Navegator from './Components/Navegator';
-import Cards from './Components/Cards';
-import { Component } from 'react';
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import home from './pages/Home'
+
 
 
 ReactDOM.render(
